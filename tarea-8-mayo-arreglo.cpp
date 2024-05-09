a7a40f9c9e01f3267b11f5d17273d2e2e6d17c2d
paopaolita <132370804+paopaolita@users.noreply.github.com>
2024-05-08 20:27:54 -0600

    arreglooooo

---
+++ tarea-8-mayo-arreglo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//Arreglos
+	int numeros[16]; // _ , _ , _ , _ , _
+	
+	cout << "Ingresa los numeros: " << endl;
+	
+	for (int i = 0; i < 16; i++) {
+		cout << "Ingrese un numero en la posicion " 
+			<< i << endl;
+		cin >> numeros[i];
+	}
+	
+	for (int i = 0; i < 16; i++) {
+		cout << "Numero ingresado en la posicion:  " 
+			<< i << ": " << numeros[i];
+	}
+	
+	return 0;
+}
+
---
+++ tarea-8-mayo-arreglo2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	const int tam1 = 1;
+	const int tam2 = 4;
+	const int tam3 = 10;
+	const int tam4 = 25;
+	const int tam5 = 17;
+	
+	int arr1[tam1];
+	int arr2[tam2];
+	int arr3[tam3];
+	int arr4[tam4];
+	int arr5[tam5];
+	
+	auto uvrmostrar = [](int* arr, int tam) {
+		for (int i = 0; i < tam; i++) {
+			cout << "Ingrese un número para el arreglo de tamaño " << tam << ": ";
+			cin >> arr[i];
+			cout << "Número ingresado: " << arr[i] << endl;
+		}
+	}; //se investigo uso de auto para facilidad https://learn.microsoft.com/es-es/cpp/cpp/auto-cpp?view=msvc-170
+	
+	
+	uvrmostrar(arr1, tam1);
+	uvrmostrar(arr2, tam2);
+	uvrmostrar(arr3, tam3);
+	uvrmostrar(arr4, tam4);
+	uvrmostrar(arr5, tam5);
+	
+	return 0;
+}
+
+
+
