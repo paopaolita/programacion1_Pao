23b151785e4e2644477f25183d11611aed20f6aa
paopaolita <132370804+paopaolita@users.noreply.github.com>
2024-05-16 22:39:51 -0600

    cifrado

---
+++ tarea-16-mayo-cifrar-letras.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string cifrar(const string& cadena) {
+	string resu = cadena;
+	int longi = resu.length();
+	
+	for (int i = 0; i < longi; i++) {
+		char cara = resu[i];
+		switch (cara) {
+		case 'i':
+		case 'I':
+			resu[i] = '1';
+			break;
+		case 'e':
+		case 'E':
+			resu[i] = '3';
+			break;
+		case 'a':
+		case 'A':
+			resu[i] = '4';
+			break;
+		case 'o':
+		case 'O':
+			resu[i] = '0';
+			break;
+		case 'u':
+		case 'U':
+			resu[i] = '*';
+			break;
+		default:
+			break;
+		}
+	}
+	
+	return resu;
+}
+
+int main() {
+	string frase, frase1;
+
+	cout << "Ingrese una palabra o frase para cifrarla: ";
+	getline(cin, frase);
+	
+	frase1 = cifrar(frase);
+	
+	cout << endl << "Frase cifrada: " << frase1 << endl;
+	
+	return 0;
+}
+
---
+++ tarea-16-mayo-cifrar-numeros.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int cifrar(int contrasena, int numero) {
+	int resultado = contrasena + numero; 
+	resultado *= numero; 
+	return resultado;
+}
+
+int main() {
+	int contrasena, numero, contrasena1;
+	
+
+	cout << "Ingrese una contraseña numérica: ";
+	cin >> contrasena;
+	
+
+	cout << "Ingrese un número para las operaciones: ";
+	cin >> numero;
+	
+
+	contrasena1 = cifrar(contrasena, numero);
+	
+	
+	cout << endl << "Contraseña cifrada: " << contrasena1 << endl;
+	
+	return 0;
+}
+
